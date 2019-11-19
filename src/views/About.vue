8714f8304799ec4302f030e8858eff6e2d5e4718
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2019-11-19 18:57:15 +0900

    add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h1>This application was built by Matsui Hidetoshi.</h1>
+      <h2>Versions</h2>
+      <h3>Frontend</h3>
+      <p>node 12.10.0</p>
+      <p>npm 6.11.3</p>
+      <p>vue-cli 3.10.0</p>
+      <p>amppfy-cli 3.0.0</p>
+      <h3>Backend</h3>
+      <p>AWS CloudFormation</p>
+      <p>AWS AppSync</p>
+      <p>AWS DynamoDB</p>
+      <p>AWS Lambda</p>
+      <p>Amazon Comprehend</p>
+      <h3>Versioning</h3>
+      <p>Github</p>
+      <h3>Architecture</h3>
+      <img src="../assets/architecture.jpg" alt="Architecture">
+    </div>
   </div>
 </template>
+<style scoped>
+div {
+  margin-bottom: 150px;
+}
+img {
+  width: 100%;
+  max-width: 480px;
+}
+</style>
