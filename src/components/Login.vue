e8f73ddbd7989e88a4e24ad97dae701dd1b78873
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2019-11-28 01:36:52 +0900

    add sign out

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="login">
-    <amplify-authenticator></amplify-authenticator>
+    <div v-if="!signedIn">
+      <amplify-authenticator></amplify-authenticator>
+    </div>
+    <div v-if="signedIn">
+      <amplify-sign-out></amplify-sign-out>
+    </div>
   </div>
 </template>
 <script>
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import { Auth } from 'aws-amplify'
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      signedIn: false
+    }
+  },
+  async beforeCreate() {
+    try {
+      await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch (err) {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      if (info === 'signedIn') {
+        this.signedIn = true
+      } else {
+        this.signedIn = false
+      }
+    })
+  }
 }
 </script>
 <style>
