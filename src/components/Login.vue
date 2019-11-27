2361f66d24be9a58ae3a894321a3a3af6ea9a066
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2019-11-28 01:10:14 +0900

    add auth

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <amplify-authenticator></amplify-authenticator>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+<style>
+.login {
+  margin: 20px;
+}
+@media screen and (max-width: 480px) {
+  .login div {
+    min-width: 0;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import Amplify from 'aws-amplify'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
 import aws_exports from './aws-exports'
 import { components } from 'aws-amplify-vue'
 
 Vue.config.productionTip = false
 
 Amplify.configure(aws_exports)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
   router,
